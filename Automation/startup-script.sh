bf1b5088f5f6f556d0de44e3846b316e8384ef04
wiqram <isaponb1>
2022-08-22 11:28:58 +0100

    jenkins file changed to run automation script.sh

--- Automation/startup-script.sh
+++ Automation/startup-script.sh
@@ -25,7 +25,9 @@ export APPS_OBJ='daemon_sets,deployments,replica_sets,stateful_sets'
 cd Automation
 echo "==> Setting up Splunk connect for Kubernetes"
 #files="kubernetes_connect_template.yaml" "deploy_sck_k8s.sh" && for each in "${files[@]}"; do wget -O- --no-check-certificate http://splunk.traderyolo.com/en-US/static/app/splunk_app_infrastructure/kubernetes_connect/"$each" > $each; done && wget https://github.com/splunk/splunk-connect-for-kubernetes/releases/download/1.3.0/splunk-connect-for-kubernetes-1.3.0.tgz -O splunk-connect-for-kubernetes.tgz && bash deploy_sck_k8s.sh
-files="kubernetes_connect_template.yaml" "deploy_sck_k8s.sh" && for each in "${files[@]}"; do wget -O- https://splunk.traderyolo.com/en-US/static/app/splunk_app_infrastructure/kubernetes_connect/"$each" > $each; done && wget https://github.com/splunk/splunk-connect-for-kubernetes/releases/download/1.3.0/splunk-connect-for-kubernetes-1.3.0.tgz -O splunk-connect-for-kubernetes.tgz && sh deploy_sck_k8s.sh
+
+#for each in "${files[@]}"; do wget -O- https://splunk.traderyolo.com/en-US/static/app/splunk_app_infrastructure/kubernetes_connect/"$each" > $each; done && wget https://github.com/splunk/splunk-connect-for-kubernetes/releases/download/1.3.0/splunk-connect-for-kubernetes-1.3.0.tgz -O splunk-connect-for-kubernetes.tgz && sh deploy_sck_k8s.sh
+sh ./deploy_sck_k8s.sh
 sleep 5 && echo " "
 
 echo "==> Build Java app image and push it to docker registry"
